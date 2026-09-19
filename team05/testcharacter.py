d1ba27ff6042ee0332031a26da305cca4400c990
antispork <fusedshadow05@gmail.com>
2026-09-19 17:40:58 -0400

    testing and beginning mapping

--- team05/testcharacter.py
+++ team05/testcharacter.py
@@ -5,8 +5,29 @@ sys.path.insert(0, '../bomberman')
 from entity import CharacterEntity
 from colorama import Fore, Back
 
+import numpy
+
 class TestCharacter(CharacterEntity):
+    def map_walls(self, wrld):
+        wall_map = numpy.zeros((wrld.height(), wrld.width()))
+        for r in range(wrld.height()):
+            for c in range(wrld.width()):
+                wall_map[r, c] = wrld.wall_at(c,r)
+        return wall_map
+
+
 
     def do(self, wrld):
         # Your code here
+        all_accessible_vars = [
+            attr for attr in dir(self)
+            if not attr.startswith('__') and not callable(getattr(self, attr))
+        ]
+        print(all_accessible_vars)
+
+        print(self.map_walls(wrld))
+        
+        self.move(0,1)
+
+        print(self.dy)
         pass
